8f18288b8e178d77f19307008c91660ef176138c
Jeeranan Tabudda <amp.2549@gmail.com>
2026-07-15 14:08:36 +0700

    feature: add main.py to preprocess and feature engineer

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ai-impact-on-students-project!")
+from src.data import load_data, clean, split_data, preprocess_data
+from src.features import engineer_features
+
+
+def run_pipeline():
+    df_raw = load_data()
+    df = clean(df_raw)
+
+    train_df, test_df = split_data(df)
+
+    train_df = preprocess_data(train_df)
+    test_df = preprocess_data(test_df)
+
+    train_df = engineer_features(train_df)
+    test_df = engineer_features(test_df)
+
+    return train_df, test_df
 
 
 if __name__ == "__main__":
-    main()
+    train_df, test_df = run_pipeline()
+    print(f"Training: {train_df.shape}")
+    print(train_df.head())
+    print(f"Testing: {test_df.shape}")
